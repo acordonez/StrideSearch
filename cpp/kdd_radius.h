1f6b638256e83f5c7039b6a121f47323f3ec9016
Matthew Mcchesney <mmcches@skybridge-login5.sandia.gov>
2017-06-02 11:44:53 -0600

    adding new files

---
+++ cpp/kdd_radius.h
@@ -0,0 +1,20 @@
+#ifndef KDD_RADIUS_H
+#define KDD_RADIUS_H
+
+#include "StrideSearchUtilities.h"
+#include "StrideSearchData_Base.h"
+#include "StrideSearchDateTime.h"
+#include "StrideSearchWorkspace.h"
+#include <vector> 
+#include <string>
+
+class kdd_radius
+{
+ public:
+ kdd_radius(Workspace2D data): nc_data(data){};
+  void runtest();
+ protected:
+  Workspace2D nc_data;
+};
+
+#endif
---
+++ cpp/kdd_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "kdd_radius.h"
+
+int main ()
+{
+  //kdd_radius kdd;
+  //kdd.runtest();
+  std::cout << "working\n";
+  return 0;
+}
