cc9e9f90c34dd0c92e4e20d6355a6c6a8fa30c5c
Matthew Mcchesney <mmcches@skybridge-login8.sandia.gov>
2017-09-18 07:38:32 -0600

    added nanoflann

---
+++ cpp/StrideSearchKdd.h
@@ -0,0 +1,13 @@
+#ifndef KDD_RADIUS_H
+#define KDD_RADIUS_H
+
+#include "StrideSearchUtilities.h" 
+#include "StrideSearch_TypeDefs.h"
+#include "nanoflann.hpp"
+#include <vector>
+#include <string>
+
+namespace StrideSearch{
+}
+
+#endif
